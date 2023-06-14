56784a3597776cda31259710bd586f17f5f8be6d
pwnfan <pwnfan@proton.me>
2023-06-14 15:33:59 +0800

    code.tests>modify.refactor: normalize variable names

--- tests/core/test_data.py
+++ tests/core/test_data.py
@@ -9,13 +9,13 @@ from tagmark.core.data import Tagmark, TagmarkItem
 from tagmark.core.github import GithubRepoInfo
 
 
-class TestTagit:
-    tagit: Tagmark = Tagmark()
+class TestTagmark:
+    tagmark_obj: Tagmark = Tagmark()
     count_added: int = 0
 
     @classmethod
     def setup_class(cls):
-        cls.count_added += cls.tagit.add(
+        cls.count_added += cls.tagmark_obj.add(
             [
                 TagmarkItem(
                     url="https://github.com/olifolkerd/tabulator",
@@ -54,21 +54,21 @@ class TestTagit:
             ],
         }
         _access_token = os.environ.get("GITHUB_TOKEN")
-        cls.tagit.get_github_repo_infos(
+        cls.tagmark_obj.get_github_repo_infos(
             access_token=_access_token, condition=ban_condition, is_ban_condition=True
         )
 
     def test_add(
         self,
     ):
-        assert self.tagit.tagmark_items[0].id == 1
-        assert self.tagit.tagmark_items[1].id == 2
-        assert self.tagit.tagmark_items[2].id == 3
+        assert self.tagmark_obj.tagmark_items[0].id == 1
+        assert self.tagmark_obj.tagmark_items[1].id == 2
+        assert self.tagmark_obj.tagmark_items[2].id == 3
         assert self.count_added == 3
 
         # test add duplicated data
         with pytest.raises(ValueError):
-            self.tagit.add(
+            self.tagmark_obj.add(
                 [
                     TagmarkItem(
                         url="https://github.com/olifolkerd/tabulator",
@@ -84,25 +84,25 @@ class TestTagit:
             )
 
     def test_count_github_url(self):
-        assert self.tagit.count_github_url == 2
+        assert self.tagmark_obj.count_github_url == 2
 
     def test_all_tags(self):
-        assert len(self.tagit.all_tags) == 8
+        assert len(self.tagmark_obj.all_tags) == 8
 
     def test_sort(
         self,
     ):
-        _sorted_tagit_items: list[TagmarkItem] = self.tagit.sort(
+        _sorted_tagit_items: list[TagmarkItem] = self.tagmark_obj.sort(
             key="url", reverse=True
         )
         assert list(_.url for _ in _sorted_tagit_items) == list(
-            reversed([_.url for _ in self.tagit.tagmark_items])
+            reversed([_.url for _ in self.tagmark_obj.tagmark_items])
         )
 
     def test_get_github_repo_infos(
         self,
     ):
-        github_repo_info: GithubRepoInfo = self.tagit.tagmark_items[0].github_repo_info
+        github_repo_info: GithubRepoInfo = self.tagmark_obj.tagmark_items[0].github_repo_info
         assert github_repo_info.url is not None
         assert github_repo_info.owner is not None
         assert github_repo_info.name is not None
@@ -119,7 +119,7 @@ class TestTagit:
         assert github_repo_info.count_release is None  # TODO
         assert github_repo_info.count_conributor is None  # TODO
 
-        github_repo_info: GithubRepoInfo = self.tagit.tagmark_items[-1].github_repo_info
+        github_repo_info: GithubRepoInfo = self.tagmark_obj.tagmark_items[-1].github_repo_info
         assert not github_repo_info
 
     def test_dump_to_json_lines(
@@ -127,7 +127,7 @@ class TestTagit:
     ):
         # test keep_empty_keys==True
         output_path: Path = Path(f"/tmp/tagit_test_{time.time()}.jsonl")
-        self.tagit.dump_to_json_lines(output_path=output_path, keep_empty_keys=True)
+        self.tagmark_obj.dump_to_json_lines(output_path=output_path, keep_empty_keys=True)
         dict_items: list[dict] = []
 
         with open(output_path, "r") as _f:
@@ -141,7 +141,7 @@ class TestTagit:
 
         # test keep_empty_keys==False
         output_path: Path = Path(f"/tmp/tagit_test_{time.time()}.jsonl")
-        self.tagit.dump_to_json_lines(output_path=output_path, keep_empty_keys=False)
+        self.tagmark_obj.dump_to_json_lines(output_path=output_path, keep_empty_keys=False)
         dict_items: list[dict] = []
 
         with open(output_path, "r") as _f:
@@ -153,7 +153,7 @@ class TestTagit:
 
         # test condition and ban_condition==True
         output_path: Path = Path(f"/tmp/tagit_test_{time.time()}.jsonl")
-        self.tagit.dump_to_json_lines(
+        self.tagmark_obj.dump_to_json_lines(
             output_path=output_path,
             keep_empty_keys=False,
             condition={
@@ -168,11 +168,11 @@ class TestTagit:
             for _line in _f:
                 dict_items.append(json.loads(_line.strip()))
         assert len(dict_items) == 1
-        assert dict_items[0]["url"] == self.tagit.tagmark_items[-1].url
+        assert dict_items[0]["url"] == self.tagmark_obj.tagmark_items[-1].url
 
         # test condition and ban_condition==False
         output_path: Path = Path(f"/tmp/tagit_test_{time.time()}.jsonl")
-        self.tagit.dump_to_json_lines(
+        self.tagmark_obj.dump_to_json_lines(
             output_path=output_path,
             keep_empty_keys=False,
             condition={
@@ -188,4 +188,4 @@ class TestTagit:
                 dict_items.append(json.loads(_line.strip()))
         assert len(dict_items) == 2
         for _dict_item in dict_items:
-            assert _dict_item["url"] != self.tagit.tagmark_items[-1].url
+            assert _dict_item["url"] != self.tagmark_obj.tagmark_items[-1].url
