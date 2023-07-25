0854e0353e1debd2d266b5849416e0ff5b58197e
pwnfan <pwnfan@proton.me>
2023-07-25 15:02:09 +0000

    core.misc>modify.refactor: change the data structure of tag json file, and making relate change to code: 1. change the way to generating gpt prompt 2. detail tag with several properties to make preparation for adding the `cheatsheet` subcommand

---
+++ tests/tools/test_checktag.py
@@ -0,0 +1,41 @@
+import pytest
+
+from tagmark.core.tag import TagItem
+from tagmark.tools.checktag import TagsChecker
+
+
+class TestTagsChecker:
+    @classmethod
+    @pytest.fixture(autouse=True)
+    def setup(cls, test_file_paths):
+        cls.tags_checker: TagsChecker = TagsChecker(
+            tagmark_data_json_path=test_file_paths["tagmark_data_json"],
+            tags_json_path=test_file_paths["tags_json"],
+            condition_json_path=test_file_paths["condition_json"],
+        )
+
+    def test_check_tags(
+        self,
+    ):
+        self.tags_checker.check_tags()
+        assert self.tags_checker.check_result.tags_only_in_data_source == {"oss"}
+        assert self.tags_checker.check_result.tags_only_in_tags_json == {"test"}
+        assert self.tags_checker.check_result.count == {
+            "tags_in_data_source": 7,
+            "tags_in_tags_json": 7,
+            "tags_only_in_data_source": 1,
+            "tags_only_in_tags_json": 1,
+        }
+
+    def test_generate_new_tag_infos(
+        self,
+    ):
+        self.tags_checker.check_tags()
+        self.tags_checker.generate_new_tag_infos()
+
+        new_tags_infos_expected: dict = self.tags_checker.tag_infos.copy()
+
+        for _tag in self.tags_checker.check_result.tags_only_in_data_source:
+            new_tags_infos_expected[_tag] = TagItem(tag=_tag).as_tags_json_data_value()
+
+        assert self.tags_checker.new_tag_infos == new_tags_infos_expected
