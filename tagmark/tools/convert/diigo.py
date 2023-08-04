7bbcda1f5c4b7b61621d9e441046fdc2e9b614c9
pwnfan <pwnfan@proton.me>
2023-08-04 05:12:02 +0000

    code.core>new: add new type `diigo_web_exported` into `convert -f`

--- tagmark/tools/convert/diigo.py
+++ tagmark/tools/convert/diigo.py
@@ -1,7 +1,10 @@
+import json
 from csv import DictReader
 from html.parser import HTMLParser
 from pathlib import Path
 from typing import Iterable
+
+from dacite import from_dict
 
 from tagmark.core import Timestamp
 from tagmark.core.convert import BaseConverter
@@ -72,3 +75,19 @@ class ExportedCsvFormatConverter(BaseConverter):
     def _convert_to_tagmark_item(self, item: dict) -> TagmarkItem:
         # TODO
         pass
+
+
+class WebExportedJsonlinesConverter(BaseConverter):
+    def _convert_to_tagmark_item(self, item: dict) -> TagmarkItem:
+        item_formatted: dict = {}
+        item_formatted["url"] = item["url"]
+        item_formatted["title"] = item["title"]
+        item_formatted["tags"] = item["tags"].split(",")
+        item_formatted["comment"] = item["description"]
+        item_formatted["time_added"] = item["created_at"]
+        return from_dict(data_class=TagmarkItem, data=item_formatted)
+
+    def load_original_items(self, data_source: Path) -> Iterable[dict]:
+        with open(data_source, "r") as _f:
+            for _json_line in _f:
+                yield json.loads(_json_line.strip())
